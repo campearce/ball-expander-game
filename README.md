602d8b9bad5d60b1c93e8bcf766ddc2038f5d3c5
Cam <cameron.pearce1@gmail.com>
2015-11-06 22:02:52 +0000

    Added basics of killer balls bouncing around the stage

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# ball-expander-game
-Ball Expansion Game - Expand the balls to cover over 2/3 of the Screen
+#Ball Expansion Game
 
-Aim of the game
+##Aim of the game
 
 The aim of the game is to expand balls in the stage area to allocate more than 2 thirds of the stage area without getting hit by the moving balls
 Level difficulty is depicted by speed of the moving the balls, number of the moving balls and size of the stage. 
 
-More changes
+##TODO
+
+Ball collides, calculate direction and velocity - http://gamedevelopment.tutsplus.com/tutorials/when-worlds-collide-simulating-circle-circle-collisions--gamedev-769
+Add Gravity to Newly Created Balls - http://codetheory.in/basics-of-implementing-gravity-with-html5-canvas/
+Balls probably need an onFrame event each to check intersects.
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,47 @@
-console.write('something');
+var c = new Shape.Circle(new Point(100, 100), 60);
+var growthRate = 1.03;
+var ballStroke = '#678';
+var ballFill = 'orange'; 
+var newBall;
+c.strokeColor = '#678';
+c.fillColor = 'hotpink';
+
+var incx = incy = 1;
+var speed = 1;
+
+view.on('frame', function() {
+	if(c.position.y + c.radius >= view.bounds.bottom) {
+		incy = -1 * speed;
+		speed++;
+	}
+	if(c.position.x + c.radius >= view.bounds.right) {
+		incx = -1 * speed;
+		speed++;
+	}
+	if(c.position.y - c.radius <= 0) {
+		incy = 1 * speed;
+		speed++;
+	}
+	if(c.position.x - c.radius <= 0) {
+		incx = 1 * speed;
+		speed++;
+	}
+	c.position += new Point(incx, incy);
+});
+
+view.on('frame', function() {
+	if (newBall) {
+		newBall.radius *= growthRate;
+	}
+});
+
+view.on('mousedown', function(event) {
+	console.log(event);
+	newBall = new Shape.Circle(new Point(event.point.x, event.point.y), 10)
+	newBall.strokeColor = ballStroke;
+	newBall.fillColor = ballFill;
+});
+
+view.on('mouseup', function(event) {
+	newBall = null;
+});
